05df848e295fce4a694db38c60818b1ff0140ac9
bazzle <barry@br-web.io>
2021-05-31 00:39:41 +0200

    select styling

--- includes/component-filter.php
+++ includes/component-filter.php
@@ -1,6 +1,6 @@
 <form class="filter">
 
-    <select name="orderby" id="orderby">
+    <select class="filter__select" name="orderby" id="orderby">
         <option class="orderby__date" value="date">Order by date</option>
         <option class="orderby__popular" value="popular">Order by most popular</option>
     </select>
